d61e9c66d33acde6913479d8080ec274ec21590a
COMBA Yannick <comba.yannick@gmail.com>
2024-04-03 09:10:07 +0200

    auth obligatoire pour envoyer msg

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get(uri:'/', action: 'App\Http\Controllers\PusherController@index');
+Route::get(uri:'/', action: 'App\Http\Controllers\PusherController@index')->middleware('auth');
 Route::post(uri:'/broadcast', action: 'App\Http\Controllers\PusherController@broadcast');
 Route::post(uri:'/receive', action: 'App\Http\Controllers\PusherController@receive');
 
